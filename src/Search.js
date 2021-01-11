b54c66b54ae3bdbc60213848b6649d355bfcb425
fbado66 <fbado66@gmail.com>
2021-01-11 13:50:14 -0500

    updating form tag in Search Component

--- src/Search.js
+++ src/Search.js
@@ -7,15 +7,15 @@ function Search(props) {
     }
 
     return (
-        <div className='searchComponent'>
+        <form className='searchComponent' name='myForm'>
             <input type='text'
                 name='search'
                 className='searchBar'
-                value={props.searchTerm}
+                // value={props.searchTerm}
                 onChange={handleResult}
                 placeholder='Search movies'/>
             <Icon id='searchIcon' size='large' color='red' name='search'/>
-        </div>
+        </form>
     )
 }
 
